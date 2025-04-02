33b98e6f2c08912dce5f85c79f2e8c1b37305a62
Joefhanie <joefhanie@gmail.com>
2025-04-02 19:46:00 +0800

    Add Create, Edit, Delete for Workers in Admin Side

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,7 +1,11 @@
-﻿using LVTS.Models;
+﻿using LVTS.Data;
+using LVTS.Models;
 using LVTS.ViewModels.Admin;
+using LVTS.ViewModels.Worker;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LVTS.Controllers
 {
@@ -9,11 +13,15 @@ namespace LVTS.Controllers
     {
         private readonly SignInManager<Admin> _signInManager;
         private readonly UserManager<Admin> _userManager;
+        private readonly UserManager<Worker> _workerUserManager;
+        private readonly LVTSContext _context;
 
-        public AdminController(SignInManager<Admin> signInManager, UserManager<Admin> userManager)
+        public AdminController(SignInManager<Admin> signInManager, UserManager<Admin> userManager, UserManager<Worker> workerUserManager, LVTSContext context)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _workerUserManager = workerUserManager;
+            _context = context;
         }
 
         public IActionResult AdminLogIn()
@@ -29,7 +37,7 @@ namespace LVTS.Controllers
                 var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, false);
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("AdminDashboard", "Admin");
                 } else
                 {
                     ModelState.AddModelError("", "Invalid Login Attempt");
@@ -45,7 +53,7 @@ namespace LVTS.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AdminSignUp(AdminSignupViewModel model)
+        public async Task<IActionResult> AdminSignUp(WorkersSignupViewModel model)
         {
             if (ModelState.IsValid)
             {
@@ -144,5 +152,190 @@ namespace LVTS.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
+
+        [Authorize]
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+
+        // Transactions
+        [Authorize]
+        public IActionResult AdminTransactions()
+        {
+            return View();
+        }
+
+        // Healthcare Workers
+        [Authorize]
+        public IActionResult AdminHealthcareWorkers()
+        {
+            var workers = _context.Workers.ToList();
+            return View(workers);
+        }
+
+        public IActionResult AdminAddWorker()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AdminAddWorker(WorkersSignupViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Check if the username already exists
+                var existingUser = await _workerUserManager.FindByNameAsync(model.Username);
+                if (existingUser != null)
+                {
+                    ModelState.AddModelError("Username", "Username already exists!");
+                    return View(model);
+                }
+
+                Worker worker = new Worker
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Age = model.Age,
+                    Address = model.Address,
+                    ContactNumber = model.ContactNumber,
+                    Email = model.Email,
+                    UserName = model.Username,
+                    Role = model.Role,
+                };
+
+                var result = await _workerUserManager.CreateAsync(worker, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("AdminHealthcareWorkers", "Admin");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            } else
+            {
+                foreach (var state in ModelState)
+                {
+                    foreach (var error in state.Value.Errors)
+                    {
+                        ModelState.AddModelError("", error.ErrorMessage);
+                    }
+                }
+            }
+                return View(model);
+        }
+
+        public async Task<IActionResult> AdminEditWorker(string id)
+        {
+            var worker = await _workerUserManager.FindByIdAsync(id);
+            if (worker == null)
+            {
+                return NotFound();
+            }
+
+            var model = new WorkersSignupViewModel
+            {
+                FirstName = worker.FirstName,
+                LastName = worker.LastName,
+                Age = worker.Age,
+                Address = worker.Address,
+                ContactNumber = worker.ContactNumber,
+                Email = worker.Email,
+                Username = worker.UserName,
+                Role = worker.Role
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AdminEditWorker(WorkersSignupViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var worker = await _context.Workers.FindAsync(model.Id);
+                if (worker == null)
+                {
+                    return NotFound();
+                }
+
+                worker.FirstName = model.FirstName;
+                worker.LastName = model.LastName;
+                worker.Age = model.Age;
+                worker.Address = model.Address;
+                worker.ContactNumber = model.ContactNumber;
+                worker.Email = model.Email;
+                worker.UserName = model.Username;
+                worker.Role = model.Role;
+
+                var updateResult = await _workerUserManager.UpdateAsync(worker);
+                if (updateResult.Succeeded)
+                {
+                    return RedirectToAction("AdminHealthcareWorkers", "Admin");
+                }
+
+                foreach (var error in updateResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            else
+            {
+                foreach (var state in ModelState)
+                {
+                    foreach (var error in state.Value.Errors)
+                    {
+                        ModelState.AddModelError("", error.ErrorMessage);
+                    }
+                }
+            }
+                return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AdminDeleteWorker(string id)
+        {
+            var worker = await _workerUserManager.FindByIdAsync(id);
+            if (worker == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _workerUserManager.DeleteAsync(worker);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("AdminHealthcareWorkers", "Admin");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return RedirectToAction("AdminHealthcareWorkers", "Admin");
+        }
+
+        // Patients
+        [Authorize]
+        public IActionResult AdminPatients()
+        {
+            return View();
+        }
+
+        // Vaccines
+        [Authorize]
+        public IActionResult AdminVaccines()
+        {
+            return View();
+        }
+
+
     }
 }
---
+++ ViewModels/Worker/WorkersSignupViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LVTS.ViewModels.Worker
+{
+    public class WorkersSignupViewModel
+    {
+        public string? Id { get; set; }
+       
+        [Required(ErrorMessage = "First Name is required.")]
+        public string FirstName { get; set; } = null!;
+       
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; } = null!;
+       
+        [Required(ErrorMessage = "Age is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Age should be higher than 0")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        public string Address { get; set; } = null!;
+
+        [Required(ErrorMessage = "Mobile Number is required.")]
+        [Phone(ErrorMessage = "Invalid Mobile Number!")]
+        public string ContactNumber { get; set; } = null!;
+
+        [Required(ErrorMessage = "Role is required.")]
+        public string Role { get; set; } = null!;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+       
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; } = null!;
+       
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at {2} and at max {1} character.")]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
